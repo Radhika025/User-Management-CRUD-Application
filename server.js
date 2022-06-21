7820c64ef91bb627ec966b76b231cf8b5580b7a2
Radhika Mittal <radhika.mittal259@gmail.com>
2022-06-21 22:05:42 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const morgan = require('morgan');
 const bodyparser = require("body-parser");
 const path = require('path');
 
+const connectDB = require('./server/database/connection');
+
 const app = express();
 
 dotenv.config( {path:'config.env'} )
@@ -11,6 +13,9 @@ const PORT = process.env.PORT || 8080
 
 //log requests
 app.use(morgan('tiny'));
+
+//mongodb connection
+connectDB();
 
 //parse requests to body-parser
 app.use(bodyparser.urlencoded({ extended : true}))
@@ -20,14 +25,12 @@ app.set("view engine", "ejs")
 //app.set("views", path.resolve(__dirname, "views/ejs"))
 
 //load my assets
+app.use("/assets", express.static('assets'))
 app.use('/css', express.static(path.resolve(__dirname, "assets/css")))
 app.use('/img', express.static(path.resolve(__dirname, "assets/img")))
 app.use('/js', express.static(path.resolve(__dirname, "assets/js")))
 
-
-app.get('/',(req,res)=>{
-    res.render('index');
-    //res.send("Crud application");
-})
+//load routers
+app.use('/', require('./server/routes/router'))
 
 app.listen(PORT, ()=> { console.log(`Server is running on http://localhost:${PORT}`)});
